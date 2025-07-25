f32c60c10de6b10bbd66ea7519c5a0e5a50cff63
Maurice Mwangi <mwangimuita18@gmail.com>
2025-07-25 21:06:13 +0300

    update services page

---
+++ src/pages/ServicesPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ServicesList from "../components/ServicesList";
+
+const ServicesPage = () => {
+  return (
+    <div className="text-center">
+      <ServicesList />
+    </div>
+  );
+};
+
+export default ServicesPage;
